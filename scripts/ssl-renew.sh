318e8a93d041304a81899fee53d1a5242bae4da0
Grant <gadecoux@gmail.com>
2022-04-08 21:09:45 -0500

    small changes

--- scripts/ssl-renew.sh
+++ scripts/ssl-renew.sh
@@ -5,5 +5,5 @@ DOCKER="/usr/bin/docker"
 
 cd /home/directus/ai-directus/
 
-$COMPOSE run certbot renew --dry-run && $COMPOSE kill -s SIGHUP webserver
+$COMPOSE run certbot renew -q && $COMPOSE kill -s SIGHUP nginx
 $DOCKER system prune -af
